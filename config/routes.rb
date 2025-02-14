aaa12e9505782c198e2c972bf9824c96e9ff7800
Asuma09 <24l1002@andrew.ac.jp>
2025-02-14 13:20:18 +0000

    dev:SNS機能を作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts//create" => "posts#create"
+  get "posts/:id" => "posts#show"
+  get "/" => "home#top"
+  get "/about" => "home#about"
+  get "/weather" => "home#weather"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
